373a98ac76166193cf29da6699f92ea01da4ad4c
martindoychinov07 <martin.v.doychinov.2021@elsys-bg.org>
2025-05-31 19:28:27 +0300

    Full swagger ready

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -11,27 +11,127 @@ module.exports = router;
 
 /**
  * @swagger
- * /api/auth/register:
+ * /api/users:
+ *   get:
+ *     summary: Get all users
+ *     responses:
+ *       200:
+ *         description: List of users
+ */
+
+/**
+ * @swagger
+ * /api/auth/signup:
  *   post:
  *     summary: Register a new user
- *     tags: [Auth]
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
  *           schema:
  *             type: object
- *             required:
- *               - username
- *               - password
  *             properties:
  *               username:
+ *                 type: string
+ *               email:
  *                 type: string
  *               password:
  *                 type: string
  *     responses:
  *       201:
- *         description: User created
- *       400:
- *         description: Bad request
+ *         description: User created and token returned
+ */
+
+/**
+ * @swagger
+ * /api/messages:
+ *   post:
+ *     summary: Send a message
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               receiverId:
+ *                 type: integer
+ *               message:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Message sent
+ */
+
+/**
+ * @swagger
+ * /api/auth/login:
+ *   post:
+ *     summary: User login
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 format: email
+ *               password:
+ *                 type: string
+ *             required:
+ *               - email
+ *               - password
+ *     responses:
+ *       200:
+ *         description: Successful login with token
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       401:
+ *         description: Invalid credentials
+ */
+
+/**
+ * @swagger
+ * /api/messages/{userId}:
+ *   get:
+ *     summary: Get messages between logged-in user and specified user
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: ID of the other user to get messages with
+ *     responses:
+ *       200:
+ *         description: List of messages
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   sender_id:
+ *                     type: integer
+ *                   receiver_id:
+ *                     type: integer
+ *                   message:
+ *                     type: string
+ *                   created_at:
+ *                     type: string
+ *                     format: date-time
+ *       401:
+ *         description: Unauthorized - user not logged in
+ *       500:
+ *         description: Server error
  */
